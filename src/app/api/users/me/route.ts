9114a9e663f0154a1528dd605415e2a2fa789f3d
E-srinibash <ellasrinibash0@gmail.com>
2024-08-08 20:09:25 +0530

    wrote code for "me" route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,23 @@
+import {connect} from "@/dbConfig/dbConfig"
+import { getDataFromToken } from "@/helpers/getDataFromToken"
+import User from "@/models/userModel"
+import { NextRequest,NextResponse } from "next/server"
+
+connect()
+
+export async function POST(request:NextRequest) {
+    //extract data from token
+
+    const userId = await getDataFromToken(request)
+    const user = await User.findOne({_id: userId}).select("-password");
+
+    if(!user){
+        return NextResponse.json({
+            message:"User not Found"
+        })
+    }
+    return NextResponse.json({
+        message:"User found",
+        data:user
+    })
+}
